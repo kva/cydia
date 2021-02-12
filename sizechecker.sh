eb7fff5e883cbafed492d5b5666582dbacf00c7a
n5 <herusularto@gmail.com>
2021-02-12 09:37:14 +0700

    add six-special/nautilus-clipboard

---
+++ sizechecker.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+# Program to check file size
+# Output from this program became input for file Packages
+# Usage, run in terminal: ./program.sh filename.deb
+
+echo -ne "Size: "
+echo   -n   $1 | wc -c $1 | awk '{print $1}'
+echo -ne "MD5sum: "
+echo   -n   $1 | md5sum $1 | awk '{print $1}'
+echo -ne "SHA1: "
+echo   -n   $1 | sha1sum $1 | awk '{print $1}'
+echo -ne "SHA256: "
+echo   -n   $1 | sha256sum $1 | awk '{print $1}'
+echo -ne "SHA512: "
+echo   -n   $1 | sha512sum $1 | awk '{print $1}'
